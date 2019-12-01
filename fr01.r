c1518a03b92c1862adcd41a07bcac1ef312aaf1c
HSHuang <hshuang88@gmail.com>
2019-12-01 12:51:53 +0800

    this is a test.

---
+++ fr01.r
@@ -0,0 +1,18 @@
+#----------------------------------
+# final report of methodology class
+# 
+# statnet and ERGM code example
+#
+# HS Huang Jan 2, 2017
+#----------------------------------
+
+# check current dir path
+getwd()
+
+# set dir path
+dir_path="d:/workspace/programming/mygit01"
+setwd(dir_path)
+getwd()
+
+
+
